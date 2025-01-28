772afc78646a1a0c4ff5fa3222351ad96387713c
Ishita Saha <161595327+ISHITASAHA01@users.noreply.github.com>
2025-01-28 21:43:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PasswordManager from './components/PasswordManager';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Password Saver</h1>
+      <PasswordManager />
+    </div>
+  );
+}
+
+export default App;
